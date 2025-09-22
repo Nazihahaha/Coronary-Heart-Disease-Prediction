f2a9cd34bb13a8e5e03210c9fb45a1a894de12d2
Nazihah Islam <140833772+Nazihahaha@users.noreply.github.com>
2025-09-22 23:33:00 +0600

    Update data source and remove unused fields

--- forms.py
+++ forms.py
@@ -3,9 +3,8 @@ import pandas as pd
 from wtforms import (SelectField, DateField, TimeField, IntegerField, SubmitField)
 from wtforms.validators import DataRequired
 
-'''train = pd.read_csv("data/train.csv")
-val = pd.read_csv("data/val.csv")
-X_data = pd.concat([train, val], axis=0).drop(columns="price")
+
+X_data = pd.read_csv("framingham.csv")
 
 
 class InputeForm(FlaskForm):
@@ -13,10 +12,7 @@ class InputeForm(FlaskForm):
                           choices = X_data.airline.unique().tolist(),
                           validators = [DataRequired()]
                           )
-    date_of_journey = DateField(
-        label="Date of Journey",
-        validators=[DataRequired()]
-    )
+    
     source = SelectField(
         label = "Source",
         choices= X_data.source.unique().tolist(),
